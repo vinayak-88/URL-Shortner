024d7e1ce3f634cdd8857604f9d0b08fc2958725
Vinayak Goel <vinayak.goel5635@gmail.com>
2025-11-13 13:22:44 +0530

    why am i doing this

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -24,7 +24,7 @@ if (process.env.NODE_ENV === "production") {
 
   app.use(express.static(distPath));
 
-  app.use((req, res) => {
+  app.get("*", (req, res) => {
     res.sendFile(path.join(distPath, "index.html"));
   });
 }
